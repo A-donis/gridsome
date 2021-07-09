db35eb64fb0cbdef7ca0c60eea699f8b14efc2de
huozl <37188836+A-donis@users.noreply.github.com>
2021-07-10 02:55:59 +0800

    's'

---
+++ src/pages/journal.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout>
+     <div class="container" >
+                <div class="journal-hero" >
+                    <h1 class="journal-header" >a wise person once said...
+      </h1>
+                </div>
+            </div>
+            <a href="/journal/gridsome-forestry-cms/" class="journal-post" >
+                <div class="container journal" >
+                    <h2 class="journal-title" >Gridsome with Forestry CMS</h2>
+                    <p class="journal-excerpt" >Get up and running with Forestry CMS, a git-backed headless CMS with a slick editing interface.</p>
+                </div>
+            </a>
+            <a href="/journal/use-gridsome-vuejs/" class="journal-post" >
+                <div class="container journal" >
+                    <h2 class="journal-title" >VueJS for your next project</h2>
+                    <p class="journal-excerpt" >Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces.</p>
+                </div>
+            </a>
+            <a href="/journal/macos-development-environment/" class="journal-post" >
+                <div class="container journal" >
+                    <h2 class="journal-title" >macOS development environment</h2>
+                    <p class="journal-excerpt" >Get up and running with a fresh macOS Catalina install</p>
+                </div>
+            </a>
+            <a href="/journal/a-journal-entry/" class="journal-post" >
+                <div class="container journal" >
+                    <h2 class="journal-title" >About Typography</h2>
+                    <p class="journal-excerpt" >Font face, vertical rythm, readability, this long fake text should flow.</p>
+                </div>
+            </a>
+  </Layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
